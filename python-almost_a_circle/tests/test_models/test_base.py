62199670633f1af849dc734fa35dc8f9b93eb181
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 16:33:16 +0000

    Fix task 0 - Base class and unit tests setup

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Unittests for Base class."""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """Unit tests for testing the Base class."""
+
+    def test_id_auto(self):
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, b2.id - 1)
+
+    def test_id_custom(self):
+        b = Base(89)
+        self.assertEqual(b.id, 89)
+
+    def test_to_json_string_none(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+
+    def test_to_json_string_empty(self):
+        self.assertEqual(Base.to_json_string([]), "[]")
+
+    def test_to_json_string_valid(self):
+        self.assertEqual(Base.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string_none(self):
+        self.assertEqual(Base.from_json_string(None), [])
+
+    def test_from_json_string_empty(self):
+        self.assertEqual(Base.from_json_string("[]"), [])
+
+    def test_from_json_string_valid(self):
+        self.assertEqual(Base.from_json_string('[{"id": 12}]'), [{'id': 12}])
+
+
+if __name__ == "__main__":
+    unittest.main()
