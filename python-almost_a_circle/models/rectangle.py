a9d9844147b843aa1421ee5101a9727c214c1785
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 17:31:39 +0000

    Add Rectangle class inheriting from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -16,84 +16,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Get width of rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Set width of rectangle."""
         self.__width = value
 
     @property
     def height(self):
+        """Get height of rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        """Set height of rectangle."""
         self.__height = value
 
     @property
     def x(self):
+        """Get x coordinate."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """Set x coordinate."""
         self.__x = value
 
     @property
     def y(self):
+        """Get y coordinate."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        """Set y coordinate."""
         self.__y = value
-
-    def area(self):
-        """Return area of rectangle."""
-        return self.__width * self.__height
-
-    def display(self):
-        """Print rectangle with '#' character."""
-        print("\n" * self.__y, end="")
-        for _ in range(self.__height):
-            print(" " * self.__x + "#" * self.__width)
-
-    def __str__(self):
-        """Return string representation."""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
-
-    def update(self, *args, **kwargs):
-        """Update attributes."""
-        attrs = ["id", "width", "height", "x", "y"]
-        if args and len(args) != 0:
-            for i, arg in enumerate(args):
-                if i < len(attrs):
-                    setattr(self, attrs[i], arg)
-        elif kwargs and len(kwargs) != 0:
-            for key, value in kwargs.items():
-                if key in attrs:
-                    setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return dictionary representation."""
-        return {
-            "id": self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y
-        }
