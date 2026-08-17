a32c816f342ab3aefcf9f1ba6a7c54c114ce4589
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 12:45:43 +0000

    Task 4 complete

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    if "/tmp" not in sys.path:
-        sys.path.append("/tmp")
-    import hidden_4
+import hidden_4
 
+
+if __name__ == "__main__":
     for name in sorted(dir(hidden_4)):
         if not name.startswith("__"):
             print(name)
