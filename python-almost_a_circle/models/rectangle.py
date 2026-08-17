59fe8aaecaa779dd4690aba6bbe2916de9e58b94
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 17:38:07 +0000

    Override __str__ method for Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,7 @@ class Rectangle(Base):
         """Print in stdout the Rectangle instance with the character #."""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Return string representation of Rectangle."""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
