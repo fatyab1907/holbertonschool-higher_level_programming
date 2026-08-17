ae168e0b21c4b3d3fcec32cc0eeee9db27307aee
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 17:40:27 +0000

    Update display method to handle x and y offsets

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -75,9 +75,10 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """Print in stdout the Rectangle instance with the character #."""
+        """Print Rectangle instance with character # taking care of x, y."""
+        print("\n" * self.__y, end="")
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Return string representation of Rectangle."""
