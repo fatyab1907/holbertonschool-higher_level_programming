eec174732d9687767aeed372dd715ac042439cd8
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 17:38:59 +0000

    Fix PEP8 line length error in Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,4 +81,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return string representation of Rectangle."""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return (
+            f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+            f"{self.__width}/{self.__height}"
+        )
