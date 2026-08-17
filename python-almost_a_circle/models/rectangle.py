2d7bb454c50b3225ef08541f8d4d143a56874e57
fatyab1907 <fatyab1907@gmail.com>
2026-08-17 17:45:16 +0000

    Add to_dictionary method to Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,13 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return dictionary representation of Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
